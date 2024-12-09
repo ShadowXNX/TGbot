f5a1fd0bec9a093669b97fec86dab8517330f35d
ShadowXNX <vnquza@gmail.com>
2024-12-10 00:38:05 +0500

    unital commit

---
+++ TG_bot.py
@@ -0,0 +1,34 @@
+# from telebot import TeleBot
+# from telebot import types
+import telebot 
+from telebot import types 
+import sqlite3 as sl
+token = '6956498084:AAHIKzeKnbFVSHfJES5PodAW8iiBBTq9p0I'
+bot = telebot.TeleBot(token)
+@bot.message_handler(commands=['start', 'help', 's'])
+def start_message(message):
+    bot.send_message(message.chat.id, "Hello! Do you have any questions?")
+    
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+    button_foo = types.InlineKeyboardButton('Support', callback_data='support')
+    button_bar = types.InlineKeyboardButton('Bar', callback_data='bar')
+    button_url = types.InlineKeyboardButton('Link', callback_data='link')
+
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(button_foo)
+    keyboard.add(button_bar)
+    keyboard.add(button_url)
+
+    bot.send_message( message.chat.id, text='Keyboard example', reply_markup=keyboard)
+@bot.message_handler(commands=['next_button'])
+def start(message):
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("🇷🇺 Русский")
+    btn2 = types.KeyboardButton('🇬🇧 English')
+    markup.add(btn1, btn2)
+    bot.send_message(message.from_user.id, "🇷🇺 Выберите язык / 🇬🇧 Choose your language", reply_markup=markup)
+bot.infinity_polling() 
+
